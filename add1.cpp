d72fb80aaf1e4ecf771aa23a77d20e85579cc118
Linos Nchena <42037657+LINOSNCHENA@users.noreply.github.com>
2019-08-25 02:42:05 +0200

    Add files via upload

---
+++ add1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ int add2(int x, int y)
+{    return x + y;  }
+
+int main(){
+    std:: cout<<"How to use two file in Cpp" << std::endl;
+    std:: cout<<"1. main file" << std::endl;    
+    std:: cout<<"2. addition file" << std::endl;
+    std::cout <<"The sum of 26 and 84 is: " << add2(266, 84) << '\n';
+    std:: cout<<"****** THE END ***********" << std::endl;
+    return 0;
+}
