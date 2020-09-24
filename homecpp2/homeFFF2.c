61ec802540aa8a710822c69617c0032961cce91c
Linos Nchena <linosnchena@gmail.com>
2020-09-24 11:42:03 +0200

    cpp and c program comperison

---
+++ homecpp2/homeFFF2.c
@@ -0,0 +1,37 @@
+//
+//
+//// C-VERSION OF FONE BOOK  v 1.2
+//
+//#include<mysql.h>
+//#include<stdio.h>
+//int main() {
+//    MYSQL* conn;
+//    MYSQL_RES* res;
+//    MYSQL_ROW row;
+//    char* server = "localhost";
+//    char* user = "root";
+//    char* password = "Monze@2019";
+//    char* database = "presly";
+//
+//    conn = mysql_init(NULL);
+//
+//    /* Connect to database */
+//    if (!mysql_real_connect(conn, server,
+//        user, password, database, 0, NULL, 0)) {
+//        fprintf(stderr, "%s\n", mysql_error(conn));
+//        return(0);
+//    }
+//    /* send SQL query */
+//    if (mysql_query(conn, "SELECT * FROM CONTACTBOOK WHERE pnumber1 > 30")) {
+//        fprintf(stderr, "%s\n", mysql_error(conn));
+//        return(0);
+//    }
+//    res = mysql_use_result(conn);
+//
+//    /* output fields 1 and 2 of each row */
+//    while ((row = mysql_fetch_row(res)) != NULL)
+//        printf("%s %s\n", row[1], row[2]);
+//    /* Release memory used to store results and close connection */
+//    mysql_free_result(res);
+//    mysql_close(conn);
+//}
